0b44725ac7a1ce8b2130eabd6a028fc1f8333597
Neel0403 <neelo2003@gmail.com>
2024-07-13 16:46:25 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const socket = io();  // sends a connection request to the backend
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0    // to eliminate caching
+        }
+    );
+}
+
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Neel"
+}).addTo(map)
+
+const markers = {};
+
+socket.on("received-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude], 16);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
